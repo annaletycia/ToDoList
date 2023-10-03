dd48da055f9d4e3d5f7872dfde960bc6716256d0
annaletycia <anna.fernandes@ufu.br>
2023-10-03 08:41:46 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./router");
 
 const app = express();
 
 app.use(express.json()); //trabalha com dados em JSON
 app.use(router); 
+app.use(cors()); //acesso liberado da API para todos
 
 module.exports = app;
